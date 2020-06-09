64f85cc669c95281bef615a953bc72598a5441b2
sijazme <sijazme@gmail.com>
2020-06-09 13:02:08 +0200

    committing changes

--- app.js
+++ app.js
@@ -30,12 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 app.set('port', process.env.PORT || 3000);
 
